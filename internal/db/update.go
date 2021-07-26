f80bd9d5b918d43301203415ba7ed22f3694c0a9
fanzhen <fanzhen-jk@360shuke.com>
2021-07-26 20:51:53 +0800

    update

--- internal/db/update.go
+++ internal/db/update.go
@@ -1,8 +1,29 @@
 package db
 
+import (
+	"fmt"
+	"strings"
+)
+
 // 组装sql
 func (m *Session) getSqlForUpdate(table table) {
-	sql := "update "
+	tableName := table.TableName()
+	sql := "update " + tableName + " set "
+	update := make([]string, 0)
+	for k := range m.UpdateSet {
+		v := m.UpdateSet[k]
+		temp := fmt.Sprintf("%s='%s'", v.column, v.value)
+		update = append(update, temp)
+	}
+	sql += strings.Join(update, ",")
+	wheres := make([]string, 0)
+	for k := range m.WhereSet {
+		v := m.WhereSet[k]
+		temp := fmt.Sprintf("`%s` %s '%s'", v.column, v.condition, v.value)
+		wheres = append(wheres, temp)
+	}
+	sql += " where " + strings.Join(wheres, " and ")
+	fmt.Println(sql)
 	m.Sql = sql
 }
 
@@ -28,5 +49,11 @@ func (m *Session) cleanUpForUpdate() {
 // update
 func (m *Session) Update(table table) {
 	m.getSqlForUpdate(table)
+	result, err := Db.Exec(m.Sql)
+	if err != nil {
+		return
+	}
+	i, _ := result.RowsAffected() // 受影响行数
+	fmt.Printf("受影响行数：%d \n", i)
 	m.cleanUpForUpdate()
 }
